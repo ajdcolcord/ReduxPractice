43c9836bda292e7ba764ce57af8b1c3546b1ea18
Austin Colcord <ajdcolcord@gmail.com>
2017-01-05 17:03:59 -0500

    Rendering 'Book List'

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Austin on 11/30/16.
+ */
+export default function() {
+    return [
+        {title: "My first book"},
+        {title: "My second book"},
+        {title: "My third"},
+        {title: "My fourth"}
+
+    ]
+}
